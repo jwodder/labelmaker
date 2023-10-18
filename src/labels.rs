bfae797ab292d952d18f7542f4fd048a3c27b96a
John T. Wodder II <git@varonathe.org>
2023-10-18 10:06:28 -0400

    impl Default for ColorSpec

--- src/labels.rs
+++ src/labels.rs
@@ -5,9 +5,23 @@ use serde_with::{serde_as, DisplayFromStr};
 
 // These are the "default colors" listed when creating a label via GitHub's web
 // UI as of 2023-09-24:
-static DEFAULT_COLORS: &[&str] = &[
-    "0052cc", "006b75", "0e8a16", "1d76db", "5319e7", "b60205", "bfd4f2", "bfdadc", "c2e0c6",
-    "c5def5", "d4c5f9", "d93f0b", "e99695", "f9d0c4", "fbca04", "fef2c0",
+static DEFAULT_COLORS: &[(u8, u8, u8)] = &[
+    (0x00, 0x52, 0xCC),
+    (0x00, 0x6B, 0x75),
+    (0x0E, 0x8A, 0x16),
+    (0x1D, 0x76, 0xDB),
+    (0x53, 0x19, 0xE7),
+    (0xB6, 0x02, 0x05),
+    (0xBF, 0xD4, 0xF2),
+    (0xBF, 0xDA, 0xDC),
+    (0xC2, 0xE0, 0xC6),
+    (0xC5, 0xDE, 0xF5),
+    (0xD4, 0xC5, 0xF9),
+    (0xD9, 0x3F, 0x0B),
+    (0xE9, 0x96, 0x95),
+    (0xF9, 0xD0, 0xC4),
+    (0xFB, 0xCA, 0x04),
+    (0xFE, 0xF2, 0xC0),
 ];
 
 type LabelName = unicase::UniCase<String>;
@@ -78,6 +92,12 @@ pub(crate) enum ColorSpec {
     Random(Vec<Color>),
 }
 
+impl Default for ColorSpec {
+    fn default() -> ColorSpec {
+        ColorSpec::Random(DEFAULT_COLORS.iter().map(|&rgb| Color::from(rgb)).collect())
+    }
+}
+
 fn color2rgbhex<S: Serializer>(color: &Color, serializer: S) -> Result<S::Ok, S::Error> {
     let [r, g, b, _] = color.to_rgba8();
     let s = format!("{:02x}{:02x}{:02x}", r, g, b);
@@ -107,5 +127,13 @@ mod tests {
         let obj = ColorContainer { color };
         let expected = format!(r#"{{"color":"{s}"}}"#);
         assert_eq!(serde_json::to_string(&obj).unwrap(), expected);
+    }
+
+    #[test]
+    fn test_default_color_spec() {
+        let ColorSpec::Random(colors) = ColorSpec::default() else {
+            panic!("ColorSpec::default() was not Random");
+        };
+        assert_eq!(colors.len(), DEFAULT_COLORS.len());
     }
 }
